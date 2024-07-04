4cb6904299ac68f07080d58ba559930ad95d618a
Dikook <vimvom931@gmail.com>
2024-07-04 14:31:18 +0500

    i did it. YEp. tab bar + table view

--- testSnapkit/Controllers/NoteBookViewController.swift
+++ testSnapkit/Controllers/NoteBookViewController.swift
@@ -6,24 +6,57 @@
 //
 
 import UIKit
+import SnapKit
 
-class NoteBookViewController: UIViewController {
-
+final class NoteBookViewController: UIViewController {
+    
+    // MARK: - Variables
+    var product = ["Входящий", "Идея", "Планы"]
+    
+    // MARK: - UI
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseID")
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setupConstraits()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Setup Views
+    func setupViews() {
+        view.addSubview(tableView)
     }
-    */
-
+    
+    // MARK: - Setup Constraits
+    func setupConstraits() {
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
+
+
+// MARK: - Extension
+extension NoteBookViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        product.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseID", for: indexPath)
+        cell.textLabel?.text = product[indexPath.row]
+        return cell
+    }
+}
+
+
