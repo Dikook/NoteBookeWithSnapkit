9bb32756cebdd68159d8e47916d09bbfe765503d
Dikook <vimvom931@gmail.com>
2024-07-06 17:20:09 +0500

    test commit

--- testSnapkit/Controllers/NoteViewController.swift
+++ testSnapkit/Controllers/NoteViewController.swift
@@ -6,24 +6,68 @@
 //
 
 import UIKit
+import SnapKit
 
 class NoteViewController: UIViewController {
-
+    
+    // MARK: - Product
+    var product = ["1", "2", "3"]
+    
+    // MARK: - UI
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(NoteViewCustomCell.self, forCellReuseIdentifier: NoteViewCustomCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Notes"
+        navigationController?.navigationBar.prefersLargeTitles = true
 
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraits()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Setup Views
+    func setupViews() {
+        view.addSubview(tableView)
     }
-    */
-
+    
+    // MARK: - Setup Constraits
+    func setupConstraits() {
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
+
+    // MARK: - Extension
+extension NoteViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NoteViewCustomCell.identifier, for: indexPath)
+        //cell.textLabel?.text = mobileBrand[indexPath.section].description[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        150
+    }
+}
+
+
---
+++ testSnapkit/Controllers/NoteViewCustomCell.swift
@@ -0,0 +1,68 @@
+//
+//  NoteViewCustomCell.swift
+//  testSnapkit
+//
+//  Created by Diar Orynbek on 06.07.2024.
+//
+
+import UIKit
+import SnapKit
+
+class NoteViewCustomCell: UITableViewCell {
+    
+    // MARK: - Identifier
+    static let identifier = "reuseID"
+    
+    
+    // MARK: - UI contructors
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "description"
+        label.font = .systemFont(ofSize: 10, weight: .semibold)
+        label.textColor = .gray
+        return label
+    }()
+    
+    // MARK: - LifeCycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+        setupLayouts()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetupViews
+    func setupViews() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+    }
+    
+    // MARK: - SetupLayouts
+    func setupLayouts() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(14)
+            make.leading.equalTo(20)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(14)
+            make.leading.equalTo(20)
+        }
+    
+    }
+    
+    // MARK: - Functions
+    public func configure(title: String, description: String) {
+        titleLabel.text = title
+        descriptionLabel.text = description
+    }
+}
